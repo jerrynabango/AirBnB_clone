bc4d26670f927af3645bac6b1df2d32b9b9f5b4f
jerrynabango <jerrynabango9@gmail.com>
2023-08-08 15:25:18 +0300

    state class

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""City details"""
+
+from models.base_model import BaseModel
+
+""""Inheritance: BaseModel"""
+
+
+class City(BaseModel):
+    """All city details"""
+    state_id = ""
+    name = ""
