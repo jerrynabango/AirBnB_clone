bad268da25c120b9e9d9b781ca0fbd6697df5952
jerrynabango <jerrynabango9@gmail.com>
2023-08-08 15:27:32 +0300

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Amenity details"""
+
+from models.base_model import BaseModel
+
+""""Inheritance: BaseModel"""
+
+
+class Amenity(BaseModel):
+    """All amenity details"""
+    name = ""
